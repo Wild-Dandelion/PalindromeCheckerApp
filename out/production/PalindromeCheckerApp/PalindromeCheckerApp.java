d8920df9f1309f4dcbd2de6960b356e353b2cb72
Wild-Dandelion <sm5756@srmist.edu.in>
2026-02-23 11:02:17 +0530

    UC4:Char

--- out/production/PalindromeCheckerApp/PalindromeCheckerApp.java
+++ out/production/PalindromeCheckerApp/PalindromeCheckerApp.java
@@ -8,6 +8,7 @@ public class PalindromeCheckerApp {
         welcomemessage();
         hardinput();
         stringReverse();
+        stringChar();
     }
 
     public static void welcomemessage() {
@@ -42,6 +43,30 @@ public class PalindromeCheckerApp {
             result = true;
         }
 
-        System.out.println("Is it a palindrome ?" + result);
+        System.out.println("Is it a palindrome ?" + result + "\n");
+    }
+
+    public static void stringChar(){
+        String input = "radar";
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length-1;
+        boolean isPalindrome = true;
+        while (start<end){
+            if (chars[start] == chars[end]){
+                end -=1;
+                start +=1;
+           }
+           else {
+                isPalindrome = false;          // Update boolean if mismatch
+                break;
+            }
+        }
+        System.out.println("Input: " + input);
+        if (isPalindrome) {
+            System.out.println("Is it a Palindrome ?: True");
+        } else {
+            System.out.println("Not a palindrome");
+        }
     }
 }
