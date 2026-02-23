eefba8ea34d7738217d45f4cf51bf2a457f362e5
Wild-Dandelion <sm5756@srmist.edu.in>
2026-02-23 10:02:36 +0530

    UC1: Add welcome message module

--- out/production/PalindromeCheckerApp/PalindromeCheckerApp.java
+++ out/production/PalindromeCheckerApp/PalindromeCheckerApp.java
@@ -15,17 +15,5 @@ public class PalindromeCheckerApp {
         System.out.println("System initialized successfully. \n");
     }
 
-    public static void hardinput() {
-        String input = "madam";
-        boolean result = false;
-
-        for(int i = 0; i < input.length() / 2; ++i) {
-            if (input.charAt(i) == input.charAt(input.length() - 1 - i)) {
-                result = true;
-            }
-        }
-
-        System.out.println("Input text: madam");
-        System.out.println("Is it a palindrome? " + result);
-    }
+    
 }
